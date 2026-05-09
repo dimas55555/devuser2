0f8d5b0f05e36638044cee3190b0e53da8389c2a
dimas55555 <144787424+dimas55555@users.noreply.github.com>
2026-05-09 13:42:51 +0300

    nwe k8s

--- migration_job/entrypoint.sh
+++ migration_job/entrypoint.sh
@@ -8,6 +8,10 @@ until pg_isready -h db -U postgres || pg_isready -h postgres -U postgres; do
   sleep 2
 done
 
+echo "Creating schema travel if not exists..."
+
+psql "$DATABASE_URL" -c "CREATE SCHEMA IF NOT EXISTS travel;"
+
 echo "Running Alembic migrations..."
 alembic upgrade head
 
